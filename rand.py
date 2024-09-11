63cac7d9a7a60fc364af1252223c117922d14d63
sharwariakre <74399676+sharwariakre@users.noreply.github.com>
2024-09-11 04:41:04 +0000

    added test cases

--- rand.py
+++ rand.py
@@ -1,10 +1,10 @@
-
-import random
+"""
+Module for generating a shuffled array.
+"""
 import secrets
 
 def random_array(arr):
-
-    shuffled_num = None
+    """Fills array with random numbers."""
     for i, _ in enumerate(arr):
         arr[i] = secrets.randbelow(21)
     return arr
