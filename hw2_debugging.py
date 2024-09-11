508b524b86226c015661d1d04c56690f7c8858c4
sharwariakre <74399676+sharwariakre@users.noreply.github.com>
2024-09-11 02:16:40 +0000

    debugged code

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -4,7 +4,7 @@ import rand
 
 def merge_sort(arr):
     """Function implementing sorting algorithm."""
-    if len(arr) == 1:
+    if len(arr) <= 1:
         return arr
 
     half = len(arr) // 2
@@ -16,20 +16,22 @@ def recombine(left_arr, right_arr):
     """Function that returns merged array"""
     left_index = 0
     right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    merge_arr = []
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
+            merge_arr.append(left_arr[left_index])
             left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
+        else:
+            merge_arr.append(right_arr[right_index])
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+    while left_index < len(left_arr):
+        merge_arr.append(left_arr[left_index])
+        left_index += 1
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    while right_index < len(right_arr):
+        merge_arr.append(right_arr[right_index])
+        right_index += 1
 
     return merge_arr
 
--- rand.py
+++ rand.py
@@ -1,14 +1,10 @@
-"""Generates a shuffled array using external 'shuf' subprocess command."""
 
-import subprocess
-
+import random
+import secrets
 
 def random_array(arr):
-    """Fills array with random numbers using 'shuf' subprocess command."""
 
     shuffled_num = None
-    for i in enumerate(arr):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
-        arr[i] = int(shuffled_num.stdout)
+    for i, _ in enumerate(arr):
+        arr[i] = secrets.randbelow(21)
     return arr
